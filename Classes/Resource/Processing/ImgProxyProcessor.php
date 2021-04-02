1a1a4fa5dcd3ffc1726c76ff11bbdae7ed112b39
Stefan Froemken <froemken@gmail.com>
2021-04-02 02:12:55 +0200

    Write processed file

--- Classes/Resource/Processing/ImgProxyProcessor.php
+++ Classes/Resource/Processing/ImgProxyProcessor.php
@@ -11,10 +11,12 @@ namespace StefanFroemken\Typo3ImageProxy\Resource\Processing;
 
 use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
 use TYPO3\CMS\Core\Core\Environment;
+use TYPO3\CMS\Core\Imaging\GraphicalFunctions;
 use TYPO3\CMS\Core\Resource\FileInterface;
 use TYPO3\CMS\Core\Resource\Processing\ProcessorInterface;
 use TYPO3\CMS\Core\Resource\Processing\TaskInterface;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Utility\MathUtility;
 use TYPO3\CMS\Core\Utility\PathUtility;
 
 /**
@@ -38,6 +40,11 @@ class ImgProxyProcessor implements ProcessorInterface
     protected $imgProxySalt = '';
 
     /**
+     * @var string
+     */
+    protected $altLocalHostName = '';
+
+    /**
      * @var int
      */
     protected $imageMaxWidth = 1024;
@@ -56,6 +63,19 @@ class ImgProxyProcessor implements ProcessorInterface
         'png'
     ];
 
+    /**
+     * @var array
+     */
+    protected $defaultConfiguration = [
+        'width' => 64,
+        'height' => 64,
+    ];
+
+    /**
+     * @var array
+     */
+    protected $configuration = [];
+
     public function __construct()
     {
         $extensionConfiguration = GeneralUtility::makeInstance(ExtensionConfiguration::class);
@@ -70,6 +90,7 @@ class ImgProxyProcessor implements ProcessorInterface
 
         $this->imgProxyKey = (string)$extensionConfiguration->get('typo3_image_proxy', 'imgProxyKey');
         $this->imgProxySalt = (string)$extensionConfiguration->get('typo3_image_proxy', 'imgProxySalt');
+        $this->altLocalHostName = (string)$extensionConfiguration->get('typo3_image_proxy', 'altLocalHostName');
         $this->imageMaxWidth = (int)$extensionConfiguration->get('typo3_image_proxy', 'maxImageWidth');
         $this->imageMaxHeight = (int)$extensionConfiguration->get('typo3_image_proxy', 'maxImageHeight');
     }
@@ -82,6 +103,18 @@ class ImgProxyProcessor implements ProcessorInterface
      */
     public function canProcessTask(TaskInterface $task)
     {
+        $sourceFile = $task->getSourceFile();
+        $this->configuration = $this->preProcessConfiguration($task->getConfiguration());
+
+        // Do not scale image if the source file has a size and the target size is larger
+        if (
+            $sourceFile->getProperty('width') > 0 && $sourceFile->getProperty('height') > 0
+            && $this->configuration['width'] > $sourceFile->getProperty('width')
+            && $this->configuration['height'] > $sourceFile->getProperty('height')
+        ) {
+            return false;
+        }
+
         return in_array($task->getTargetFileExtension(), $this->allowedImageExtensions, true);
     }
 
@@ -92,14 +125,13 @@ class ImgProxyProcessor implements ProcessorInterface
      */
     public function processTask(TaskInterface $task)
     {
+        // early return, if binary representation of key/salt are invalid
         $keyBin = pack("H*" , $this->imgProxyKey);
-        if (empty($keyBin)) {
-            \TYPO3\CMS\Core\Utility\DebugUtility::debug('Key expected to be hex-encoded string');
-        }
-
         $saltBin = pack("H*" , $this->imgProxySalt);
-        if (empty($saltBin)) {
-            \TYPO3\CMS\Core\Utility\DebugUtility::debug('Salt expected to be hex-encoded string');
+        if (empty($keyBin) || empty($saltBin)) {
+            \TYPO3\CMS\Core\Utility\DebugUtility::debug('Key or Salt expected to be hex-encoded string');
+            $task->setExecuted(false);
+            return;
         }
 
         $publicUrlOfImage = $this->getPublicUrlOfImage($task->getSourceFile());
@@ -108,8 +140,8 @@ class ImgProxyProcessor implements ProcessorInterface
         $path = sprintf(
             '/%s/%d/%d/%s/%d/%s.%s',
             'fit',
-            $this->imageMaxWidth,
-            $this->imageMaxHeight,
+            $this->configuration['width'],
+            $this->imageMaxHeight['height'],
             'no',
             0,
             rtrim(strtr(base64_encode($publicUrlOfImage), '+/', '-_'), '='),
@@ -118,11 +150,9 @@ class ImgProxyProcessor implements ProcessorInterface
 
         $signature = rtrim(strtr(base64_encode(hash_hmac('sha256', $saltBin . $path, $keyBin, true)), '+/', '-_'), '=');
 
-        /*$task->getTargetFile()->setName($task->getTargetFileName());
-        $task->getTargetFile()->updateProperties(
-            ['width' => $imageDimensions[0], 'height' => $imageDimensions[1], 'size' => filesize($result['filePath']), 'checksum' => $task->getConfigurationChecksum()]
-        );
-        $task->getTargetFile()->setContents(
+        $temporaryFilePath = $this->getTemporaryFilePath($task);
+        file_put_contents(
+            $temporaryFilePath,
             GeneralUtility::getUrl(
                 sprintf(
                     '%s%s%s',
@@ -131,17 +161,20 @@ class ImgProxyProcessor implements ProcessorInterface
                     $path
                 )
             )
-        );*/
-
-        \TYPO3\CMS\Core\Utility\DebugUtility::debug(
-            sprintf(
-                '%s%s%s',
-                $this->imgProxyUrl,
-                $signature,
-                $path
-            ),
-            'Signed ImgProxy Path'
         );
+
+        $task->setExecuted(true);
+        $imageDimensions = $this->getGraphicalFunctionsObject()->getImageDimensions($temporaryFilePath);
+        $task->getTargetFile()->setName($task->getTargetFileName());
+        $task->getTargetFile()->updateProperties(
+            [
+                'width' => $imageDimensions[0],
+                'height' => $imageDimensions[1],
+                'size' => filesize($temporaryFilePath),
+                'checksum' => $task->getConfigurationChecksum()
+            ]
+        );
+        $task->getTargetFile()->updateWithLocalFile($temporaryFilePath);
     }
 
     protected function getPublicUrlOfImage(FileInterface $sourceFile): string
@@ -160,9 +193,46 @@ class ImgProxyProcessor implements ProcessorInterface
 
         return sprintf(
             '%s/%s%s',
-            GeneralUtility::getIndpEnv('TYPO3_REQUEST_HOST'),
+            $this->altLocalHostName ?: GeneralUtility::getIndpEnv('TYPO3_REQUEST_HOST'),
             $pathPart,
             $filePart
         );
+    }
+
+    /**
+     * Enforce default configuration for preview processing
+     *
+     * @param array $configuration
+     * @return array
+     */
+    public function preProcessConfiguration(array $configuration): array
+    {
+        $configuration = array_replace($this->defaultConfiguration, $configuration);
+        $configuration['width'] = MathUtility::forceIntegerInRange($configuration['width'], 1, $this->imageMaxWidth);
+        $configuration['height'] = MathUtility::forceIntegerInRange($configuration['height'], 1, $this->imageMaxHeight);
+
+        return array_filter(
+            $configuration,
+            function ($value, $name) {
+                return !empty($value) && in_array($name, ['width', 'height'], true);
+            },
+            ARRAY_FILTER_USE_BOTH
+        );
+    }
+
+    /**
+     * Returns the path to a temporary file for processing
+     *
+     * @param TaskInterface $task
+     * @return string
+     */
+    protected function getTemporaryFilePath(TaskInterface $task)
+    {
+        return GeneralUtility::tempnam($task->getName(), '.' . $task->getTargetFileExtension());
+    }
+
+    protected function getGraphicalFunctionsObject(): GraphicalFunctions
+    {
+        return GeneralUtility::makeInstance(GraphicalFunctions::class);
     }
 }
