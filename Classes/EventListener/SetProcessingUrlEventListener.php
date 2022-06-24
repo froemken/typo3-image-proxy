f861688b0e50ae5f35b5ae6cf4b20e8725645360
Stefan Froemken <froemken@gmail.com>
2022-06-25 00:36:00 +0200

    Move code into its own ImgProxyService class

---
+++ Classes/EventListener/SetProcessingUrlEventListener.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the package stefanfroemken/typo3-image-proxy.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace StefanFroemken\Typo3ImageProxy\EventListener;
+
+use StefanFroemken\Typo3ImageProxy\Service\ImgProxyService;
+use TYPO3\CMS\Core\Resource\Event\BeforeFileProcessingEvent;
+
+/**
+ * Set processing URL of processed image
+ */
+class SetProcessingUrlEventListener
+{
+    /**
+     * @var ImgProxyService
+     */
+    protected $imgProxyService;
+
+    public function __construct(ImgProxyService $imgProxyService)
+    {
+        $this->imgProxyService = $imgProxyService;
+    }
+
+    public function __invoke(BeforeFileProcessingEvent $event): void
+    {
+        $publicUrl = $this->imgProxyService->getProcessingUrl(
+            $event->getFile(),
+            $event->getProcessedFile(),
+            $event->getConfiguration()
+        );
+
+        // If publicUrl is empty we already create a log entry in ImgProxyService
+        if ($publicUrl) {
+            $event->getProcessedFile()->updateProcessingUrl($publicUrl);
+        }
+    }
+}
--- ext_localconf.php
+++ ext_localconf.php
@@ -2,10 +2,22 @@
 
 defined('TYPO3_MODE') or die();
 
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['fal']['processors']['ImgProxyProcessor'] = [
-    'className' => \StefanFroemken\Typo3ImageProxy\Resource\Processing\ImgProxyProcessor::class,
-    'before' => [
-        'LocalImageProcessor',
-        'DeferredBackendImageProcessor'
-    ]
-];
+call_user_func(static function () {
+    if (!isset($GLOBALS['TYPO3_CONF_VARS']['LOG']['StefanFroemken']['Typo3ImageProxy']['writerConfiguration'])) {
+        $GLOBALS['TYPO3_CONF_VARS']['LOG']['StefanFroemken']['Typo3ImageProxy']['writerConfiguration'] = [
+            \Psr\Log\LogLevel::ERROR => [
+                \TYPO3\CMS\Core\Log\Writer\FileWriter::class => [
+                    'logFileInfix' => 'imgproxy',
+                ],
+            ],
+        ];
+    }
+
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['fal']['processors']['ImgProxyProcessor'] = [
+        'className' => \StefanFroemken\Typo3ImageProxy\Resource\Processing\ImgProxyProcessor::class,
+        'before' => [
+            'LocalImageProcessor',
+            'DeferredBackendImageProcessor'
+        ]
+    ];
+});
